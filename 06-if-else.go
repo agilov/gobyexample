e5724a36ff42e042423a6051f65e447be076cf1f
agilov <agilovr@gmail.com>
2022-02-02 13:46:58 +0700

    Maps and gofmt

--- 06-if-else.go
+++ 06-if-else.go
@@ -3,30 +3,30 @@ package main
 import "fmt"
 
 // Branching with if and else in Go is straight-forward.
-func main(){
+func main() {
 
-    // Here’s a basic example.
-    if 7%2 == 0 {
-        fmt.Println("7 is even")
-    } else {
-        fmt.Println("7 is odd")
-    }
+	// Here’s a basic example.
+	if 7%2 == 0 {
+		fmt.Println("7 is even")
+	} else {
+		fmt.Println("7 is odd")
+	}
 
-    // You can have an if statement without an else.
-    if 8%4 == 0 {
-        fmt.Println("8 is divisible by 4")
-    }
+	// You can have an if statement without an else.
+	if 8%4 == 0 {
+		fmt.Println("8 is divisible by 4")
+	}
 
-    // A statement can precede conditionals; any variables declared in this statement are available in all branches.
-    if n := 9; n < 0 {
-        fmt.Println(n, " is negative")
-    } else if n > 10 {
-        fmt.Println(n, " has 1 digit")
-    } else {
-        fmt.Println(n, " has multiple digits")
-    }
+	// A statement can precede conditionals; any variables declared in this statement are available in all branches.
+	if n := 9; n < 0 {
+		fmt.Println(n, " is negative")
+	} else if n > 10 {
+		fmt.Println(n, " has 1 digit")
+	} else {
+		fmt.Println(n, " has multiple digits")
+	}
 
-    fmt.Println("There is no ternary if in Go, so you’ll need to use a full if statement even for basic conditions.")
+	fmt.Println("There is no ternary if in Go, so you’ll need to use a full if statement even for basic conditions.")
 }
 
 // Note that you don’t need parentheses around conditions in Go, but that the braces are required.
--- 08-arrays.go
+++ 08-arrays.go
@@ -2,36 +2,36 @@ package main
 
 import "fmt"
 
-func main(){
-    // Here we create an array a that will hold exactly 5 ints.
-    // The type of elements and length are both part of the array’s type.
-    // By default an array is zero-valued, which for ints means 0s.
-    var a [5]int
-    fmt.Println("emp:", a)
+func main() {
+	// Here we create an array a that will hold exactly 5 ints.
+	// The type of elements and length are both part of the array’s type.
+	// By default an array is zero-valued, which for ints means 0s.
+	var a [5]int
+	fmt.Println("emp:", a)
 
-    // We can set a value at an index using the array[index] = value syntax,
-    // and get a value with array[index].
-    a[4] = 100
-    fmt.Println("set:", a)
-    fmt.Println("get:", a[4])
+	// We can set a value at an index using the array[index] = value syntax,
+	// and get a value with array[index].
+	a[4] = 100
+	fmt.Println("set:", a)
+	fmt.Println("get:", a[4])
 
-    // The builtin len returns the length of an array.
-    fmt.Println("len:", len(a))
+	// The builtin len returns the length of an array.
+	fmt.Println("len:", len(a))
 
-    // Use this syntax to declare and initialize an array in one line.
-    b := [5]int{-1,1,2,3,4}
-    fmt.Println("dcl:", b)
+	// Use this syntax to declare and initialize an array in one line.
+	b := [5]int{-1, 1, 2, 3, 4}
+	fmt.Println("dcl:", b)
 
-    // Array types are one-dimensional, but you can compose types to build multi-dimensional data structures.
-    var twoD [2][3][4]int
-    for i := 0; i < 2; i++ {
-        for j := 0; j < 3; j++ {
-            for k := 0; k < 4; k++ {
-                twoD[i][j][k] = i + j + k
-            }
-        }
-    }
+	// Array types are one-dimensional, but you can compose types to build multi-dimensional data structures.
+	var twoD [2][3][4]int
+	for i := 0; i < 2; i++ {
+		for j := 0; j < 3; j++ {
+			for k := 0; k < 4; k++ {
+				twoD[i][j][k] = i + j + k
+			}
+		}
+	}
 
-    // Note that arrays appear in the form [v1 v2 v3 ...] when printed with fmt.Println.
-    fmt.Println("twoD:", twoD)
+	// Note that arrays appear in the form [v1 v2 v3 ...] when printed with fmt.Println.
+	fmt.Println("twoD:", twoD)
 }
