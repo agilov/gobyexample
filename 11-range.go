47636787b52e043b1b9788085a2421a2456b9263
agilov <agilovr@gmail.com>
2022-02-02 17:35:09 +0700

    all before pointers

--- 11-range.go
+++ 11-range.go
@@ -4,39 +4,39 @@ import "fmt"
 
 // range iterates over elements in a variety of data structures.
 // Let’s see how to use range with some of the data structures we’ve already learned.
-func main(){
-    // Here we use range to sum the numbers in a slice. Arrays work like this too.
-    nums := []int{2,3,4}
-    sum := 0
-    for _, n := range nums {
-        sum += n
-    }
-    fmt.Println("sum:", sum)
+func main() {
+	// Here we use range to sum the numbers in a slice. Arrays work like this too.
+	nums := []int{2, 3, 4}
+	sum := 0
+	for _, n := range nums {
+		sum += n
+	}
+	fmt.Println("sum:", sum)
 
-    // range on arrays and slices provides both the index and value for each entry.
-    // Above we didn't need the index, so we ignored it with the blank identifier _.
-    // Sometimes we actually want the indexes though
-    for i, v := range nums {
-        if v == 3 {
-            fmt.Println("index of 3:", i)
-        }
-    }
+	// range on arrays and slices provides both the index and value for each entry.
+	// Above we didn't need the index, so we ignored it with the blank identifier _.
+	// Sometimes we actually want the indexes though
+	for i, v := range nums {
+		if v == 3 {
+			fmt.Println("index of 3:", i)
+		}
+	}
 
-    // range on map iterates over key/value pairs
-    kvs := map[string]string{"a": "apple", "b": "banana"}
-    for k, v := range kvs {
-        fmt.Printf("%s -> %s\n", k, v)
-    }
+	// range on map iterates over key/value pairs
+	kvs := map[string]string{"a": "apple", "b": "banana"}
+	for k, v := range kvs {
+		fmt.Printf("%s -> %s\n", k, v)
+	}
 
-    // range can also iterate ofer just the keys of a map
-    for k := range kvs {
-        fmt.Println("key:", k)
-    }
+	// range can also iterate ofer just the keys of a map
+	for k := range kvs {
+		fmt.Println("key:", k)
+	}
 
-    // range on strings iterates over Unicode code points.
-    // The first value is the starting byte index of the rune
-    // and the second the rune itself
-    for i, c := range " !\"#$%&'()*+,-./" {
-        fmt.Println(i, c)
-    }
+	// range on strings iterates over Unicode code points.
+	// The first value is the starting byte index of the rune
+	// and the second the rune itself
+	for i, c := range " !\"#$%&'()*+,-./" {
+		fmt.Println(i, c)
+	}
 }
---
+++ 16-recursion.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+
+// Go supports [recursive functions](https://en.wikipedia.org/wiki/Recursion_(computer_science))
+// here's a classic example.
+
+// This `fact` function calls itself until it reaches base case of `fact(0)`
+func fact(num int) int {
+	if num == 0 {
+		return 1
+	}
+
+	return num * fact(num-1)
+}
+
+func main() {
+	fmt.Println(fact(7))
+
+	// Closures can also be recursive, but this requires the closure to be declared
+	// with a typed var explicitly before it's defined.
+	var fib func(n int) int
+
+	fib = func(n int) int {
+		if n < 2 {
+			return n
+		}
+
+		// Since fib was previously declared in main,
+		// Go knows which function to call with fib here.
+		return fib(n-1) + fib(n-2)
+	}
+
+	fmt.Println(fib(7))
+}
