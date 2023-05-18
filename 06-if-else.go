f864443733540c84a2ad5170be2d76ede4aec0d4
agilov <agilovr@gmail.com>
2023-05-18 19:52:50 +0300

    conditionals

--- 06-if-else.go
+++ 06-if-else.go
@@ -4,12 +4,13 @@ import "fmt"
 
 // Branching with if and else in Go is straight-forward.
 func main() {
+	eight := 8
 
 	// Here’s a basic example.
-	if 7%2 == 0 {
-		fmt.Println("7 is even")
+	if eight%2 == 0 {
+		fmt.Printf("%d is even\n", eight)
 	} else {
-		fmt.Println("7 is odd")
+		fmt.Printf("%d is odd\n", eight)
 	}
 
 	// You can have an if statement without an else.
@@ -19,11 +20,11 @@ func main() {
 
 	// A statement can precede conditionals; any variables declared in this statement are available in all branches.
 	if n := 9; n < 0 {
-		fmt.Println(n, " is negative")
-	} else if n > 10 {
-		fmt.Println(n, " has 1 digit")
+		fmt.Println("n is negative")
+	} else if n < 10 {
+		fmt.Println("n has 1 digit")
 	} else {
-		fmt.Println(n, " has multiple digits")
+		fmt.Println("n has multiple digits")
 	}
 
 	fmt.Println("There is no ternary if in Go, so you’ll need to use a full if statement even for basic conditions.")
