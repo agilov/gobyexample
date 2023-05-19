269aa32fdc1456d66e70d0e1d89d4432d0c968ea
agilov <agilovr@gmail.com>
2023-05-19 19:07:26 +0300

    range

--- 11-range.go
+++ 11-range.go
@@ -6,7 +6,7 @@ import "fmt"
 // Let’s see how to use range with some of the data structures we’ve already learned.
 func main() {
 	// Here we use range to sum the numbers in a slice. Arrays work like this too.
-	nums := []int{2, 3, 4}
+	nums := []int{1, 2, 3}
 	sum := 0
 	for _, n := range nums {
 		sum += n
@@ -16,27 +16,27 @@ func main() {
 	// range on arrays and slices provides both the index and value for each entry.
 	// Above we didn't need the index, so we ignored it with the blank identifier _.
 	// Sometimes we actually want the indexes though
-	for i, v := range nums {
-		if v == 3 {
-			fmt.Println("index of 3:", i)
+	for i, n := range nums {
+		if n == 2 {
+			fmt.Println("index of 2:", i)
 		}
 	}
 
 	// range on map iterates over key/value pairs
-	kvs := map[string]string{"a": "apple", "b": "banana"}
+	kvs := map[string]string{"e": "elmali", "k": "karpuz"}
 	for k, v := range kvs {
 		fmt.Printf("%s -> %s\n", k, v)
 	}
 
 	// range can also iterate ofer just the keys of a map
 	for k := range kvs {
-		fmt.Println("key:", k)
+		fmt.Printf("key: %s\n", k)
 	}
 
 	// range on strings iterates over Unicode code points.
 	// The first value is the starting byte index of the rune
 	// and the second the rune itself
-	for i, c := range " !\"#$%&'()*+,-./" {
-		fmt.Println(i, c)
+	for byte, rune := range "mywhalalla!" {
+		fmt.Println(byte, rune)
 	}
 }
